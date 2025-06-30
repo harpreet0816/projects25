7471015f38f06904abf6da81b559000e2ac674ce
harpreet0816 <harpreet8730@gmail.com>
2025-07-01 01:02:51 +0530

    update frontend to make connection with backend

--- websockets/frontend/src/App.tsx
+++ websockets/frontend/src/App.tsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>hello</div>;
+  const [socket, setSocket] = useState<null | WebSocket>(null);
+  const [latestMessage, setLatestMessage] = useState([]);
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    socket.onopen = () => {
+      console.log("Connected");
+      setSocket(socket);
+    };
+
+    socket.onmessage = (message) => {
+      console.log("Recieved message", message.data);
+      setLatestMessage(message.data);
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  if (!socket) return <div>Loading...</div>;
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(event) => {
+          setMessage(event?.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          socket.send(message);
+        }}
+      >
+        Send
+      </button>
+      <span>{latestMessage}</span>
+    </div>
+  );
 }
 
 export default App;
