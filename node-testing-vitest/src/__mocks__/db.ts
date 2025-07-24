847b541c56e966fced6a2a36c04bb110e9bd09dc
harpreet0816 <harpeet8730@gmail.com>
2025-07-25 00:16:12 +0530

    revert "testing"
    This reverts commit adde3b8195115b70908c6073dc17a33f4e5069e3.

--- node-testing-vitest/src/__mocks__/db.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-import { beforeEach } from 'vitest'
-import { mockDeep, mockReset } from 'vitest-mock-extended'
-
-export const prismaClient = mockDeep<PrismaClient>()
